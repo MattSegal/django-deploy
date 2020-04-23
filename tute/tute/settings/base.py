211ec23b0dd134b9b9fbc70ab93ef00a0fb9a25f
Matthew Segal <mattdsegal@gmail.com>
2020-04-23 16:10:16 +1000

    Fix database path

--- tute/tute/settings/base.py
+++ tute/tute/settings/base.py
@@ -64,10 +64,11 @@ WSGI_APPLICATION = 'tute.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
+PARENT_DIR = os.path.dirname(BASE_DIR)
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': os.path.join(PARENT_DIR, 'db.sqlite3'),
     }
 }
 
